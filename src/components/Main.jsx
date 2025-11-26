7d6c9b28e6fccf6c88885d5917b3d260a6f7efd4
Anonymous20058 <tanvirhossien13@gmail.com>
2025-11-26 23:10:02 +0600

    commit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,7 +3,7 @@ import { Hero } from "./Hero";
 
 const Main = () => {
   return (
-    <main className="container mx-auto h-screen rounded-2xl bg-[#20242f] shadow-2xl shadow-gray-900">
+    <main className="container mx-auto h-[100vh-200px] md:h-screen rounded-2xl bg-[#20242f] shadow-2xl shadow-gray-900">
       <Hero />
     </main>
   );
