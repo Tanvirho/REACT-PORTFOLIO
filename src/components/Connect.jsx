c9fd9a808c42eabe872a7c67d4972bd77c7eee10
Anonymous20058 <tanvirhossien13@gmail.com>
2025-12-12 20:09:30 +0600

    add class

--- src/components/Connect.jsx
+++ src/components/Connect.jsx
@@ -7,7 +7,7 @@ export const Connect = () => {
     <div className="container mx-auto rounded-2xl bg-linear-to-r from-[#00BCD4] to-[#8E2DE2] p-0.5">
       <div className="w-full rounded-2xl bg-[#191d26] p-3">
         <div className="mb-15 mt-6 text-center">
-        <h1 className="relative inline-block border-b-2 border-[#00BCD4] bg-[#191d26] text-4xl font-medium text-[#00BCD4]">
+        <h1 className="relative inline-block border-b-2 border-[#00BCD4] bg-[#191d26] text-3xl md:text-4xl font-medium text-[#00BCD4]">
           Get In Touch
         </h1>
       </div>
