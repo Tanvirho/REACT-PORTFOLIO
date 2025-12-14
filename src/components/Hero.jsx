b7c267f62a6331b1852a018b0f779e0826ae4189
Anonymous20058 <tanvirhossien13@gmail.com>
2025-12-14 11:10:11 +0600

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ import { HeroSidebar } from "./HeroSidebar";
 export const Hero = () => {
   const About = lazy(()=>import('./About'))
   return (
-    <div className="flex h-auto items-center justify-around md:h-screen">
+    <div className="flex h-auto items-center justify-around md:min-h-[768px] md:max-h-[800px]">
       <HeroEffect />
       <HeroSidebar />
     </div>
